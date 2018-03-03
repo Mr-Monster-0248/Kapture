b4c28dfd1c8603eceb386e6f2c52b1e22d36accb
Unknown <vincent.lacrouts@laposte.net>
2018-03-03 20:56:11 +0100

    Read map from file
    Add map.txt and field.xlsx(to preview the map)

--- test_v.c
+++ test_v.c
@@ -5,28 +5,42 @@ Square** init_map();
 int main(int argc, char *argv[])
 {
     Square **map = init_map();
+
     return 0;
 }
 
 Square** init_map()
 {
-    int i, j;
+    int i, j, k, x;
     Square **map = (Square**) malloc(NBR_CASE_Y * sizeof(Square*));
 
+    FILE* fichier = NULL;
+    fichier = fopen("map.txt","r");
+
+    if (fichier == NULL)
+    {
+        printf("il manque le fichier map.txt");
+    }
+
+
+    k = 0;
     for (i = 0; i < NBR_CASE_Y; i++)
     {
         map[i] = (Square*) malloc(NBR_CASE_X * sizeof(Square));
         for (j = 0; j < NBR_CASE_X; j++)
         {
-            map[i][j].field = NORMAL;
+            x = fgetc(fichier);
+            map[i][j].field = x-48;
             map[i][j].pawn.team = 0;
             map[i][j].pawn.type = EMPTY;
             map[i][j].pawn.flag = FALSE;
             map[i][j].pawn.actionPoint = 0;
             map[i][j].visible_red = 0;
             map[i][j].visible_blue = 0;
+            printf("%d",x-48);
         }
+        fgetc(fichier);
     }
-
+    fclose(fichier);
     return map;
 }
