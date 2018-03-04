1f108e9f1b6349b6a3be3a1e538d0206fd189b86
Unknown <vincent.lacrouts@laposte.net>
2018-03-04 11:38:50 +0100

    Added function move_pawn()

--- headers/game.h
+++ headers/game.h
@@ -12,9 +12,11 @@
     #define NBR_SHOCK 1
     #define NBR_MEMBER NBR_SCOUT + NBR_INFANTRYMAN + NBR_SHOCK
 
-    //
+    // Main game fonction
     void game(SDL_Surface *screen, Square **map, Player **players);
 
+    // Move a pawn to an other position
+    void move_pawn(Player** players, Square** map, SDL_Rect prev_loc, SDL_Rect actual_loc);
 
 
 #endif
--- sources/game.c
+++ sources/game.c
@@ -9,3 +9,21 @@ void game(SDL_Surface *screen, Square **map, Player **players)
     display_field(screen, map);
     display_infobar(screen, players[0], 1);
 }
+
+void move_pawn(Player** players, Square** map, SDL_Rect prev_loc, SDL_Rect actual_loc)
+{
+    int team, id;
+    team = map[prev_loc.y][prev_loc.x].pawn.team;
+    id = map[prev_loc.y][prev_loc.x].pawn.id;
+    players[team][id].position.x = actual_loc.x;
+    players[team][id].position.y = actual_loc.y;
+
+    map[actual_loc.y][actual_loc.x].pawn.id = map[prev_loc.y][prev_loc.x].pawn.id;
+    map[actual_loc.y][actual_loc.x].pawn.team = map[prev_loc.y][prev_loc.x].pawn.team;
+    map[actual_loc.y][actual_loc.x].pawn.type = map[prev_loc.y][prev_loc.x].pawn.type;
+
+    map[prev_loc.y][prev_loc.x].pawn.type = EMPTY;
+
+
+
+}
